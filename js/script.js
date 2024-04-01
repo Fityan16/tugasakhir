34c9cf777f539938855b0cb78f05185d02a521f8
Fityan16 <fityangithub@gmail.com>
2024-04-01 23:37:46 +0700

    membuat search form

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-// Toggle Class Active
+// Toggle Class Active untuk hamburger
 
 const navbarNav = document.querySelector(".navbar-nav");
 
@@ -8,12 +8,27 @@ document.querySelector("#hamburger-menu").onclick = () => {
   navbarNav.classList.toggle("active");
 };
 
-// Klik di luar sidebar untuk menghilangkan nav
+// toggle class active untuk search form
 
-const hamburger = document.querySelector("#hamburger-menu");
+const searchForm = document.querySelector(".search-form");
+const searchBox = document.querySelector("#search-box");
+
+document.querySelector("#search-button").onclick = (e) => {
+  searchForm.classList.toggle("active");
+  searchBox.focus();
+  e.preventDefault();
+};
+
+// Klik di luar sidebar untuk menghilangkan Hamburger menu
+
+const hm = document.querySelector("#hamburger-menu");
+const sb = document.querySelector('#search-button');
 
 document.addEventListener("click", function (e) {
-  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+  if (!hm.contains(e.target) && !navbarNav.contains(e.target)) {
     navbarNav.classList.remove("active");
+  }
+  if (!sb.contains(e.target) && !searchForm.contains(e.target)) {
+    searchForm.classList.remove("active");
   }
 });
